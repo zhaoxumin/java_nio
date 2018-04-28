ee1761367d8beb8482a2e86dd1d09157cc8125b4
zhaoxumin <13764162650@163.com>
2018-04-28 19:54:25 +0800

    写进磁盘内存中

--- src/com/java/javanio/TestChannel.java
+++ src/com/java/javanio/TestChannel.java
@@ -4,7 +4,10 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class TestChannel {
     public void test1(){
@@ -63,9 +66,43 @@ public class TestChannel {
 
     }
 
+    public void test2(){
+        FileChannel inchannel =null;
+        FileChannel outchannel =null;
+        try {
+            inchannel = FileChannel.open(Paths.get("/Users/zhaoxumin/java_nio/1.jpg"), StandardOpenOption.READ);
+            outchannel = FileChannel.open(Paths.get("/Users/zhaoxumin/java_nio/2.jpg"), StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+            MappedByteBuffer inMappedBuf = inchannel.map(FileChannel.MapMode.READ_ONLY, 0, inchannel.size());
+            MappedByteBuffer outMappedBuf = outchannel.map(FileChannel.MapMode.READ_WRITE, 0, inchannel.size());
+            byte[] dst = new byte[inMappedBuf.limit()];
+            inMappedBuf.get(dst);
+            outMappedBuf.put(dst);
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            if(inchannel!=null) {
+                try {
+                    inchannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(outchannel!=null) {
+                try {
+                    outchannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
     public static void main(String []args){
         TestChannel testChannel=new TestChannel();
         testChannel.test1();
+        testChannel.test2();
     }
 
 }
